9c59927fa85f8eaa150c6b1e10b394c91df0ad1a
lxlfpeng <565289282@qq.com>
2024-11-07 20:11:45 +0800

    完成添加

--- run.py
+++ run.py
@@ -8,22 +8,22 @@ parser.add_argument("--email_pass", type=str, default="", help='input email_pass
 os.environ['email_pwd'] = parser.parse_args().email_pass
 
 
-# # 周三爬取排行榜和选手数据
-# if datetime.now().weekday() == 2:
-#     os.system("scrapy crawl ranking")
-#     os.system("scrapy crawl athlete")
-#
-# # 周三爬取已进行比赛的数据
-# if datetime.now().weekday() == 6:
-#     os.system("scrapy crawl eventpass")
-#
-# # 每日爬取赛程
-# os.system("scrapy crawl upcoming")
+# 周三爬取排行榜和选手数据
+if datetime.now().weekday() == 2:
+    os.system("scrapy crawl ranking")
+    os.system("scrapy crawl athlete")
+
+# 周天爬取已进行比赛的数据
+if datetime.now().weekday() == 6:
+    os.system("scrapy crawl eventpass")
+
+# 每日爬取赛程
+os.system("scrapy crawl upcoming")
 
 # os.system("scrapy crawl upcoming")
 # os.system("scrapy crawl eventpass")
 # os.system("scrapy crawl ranking")
-os.system("scrapy crawl athlete")
+# os.system("scrapy crawl athlete")
 # # os.system("scrapy crawl eventpass -a category=全量")
 
 # 取出所有日志,组成字符串
